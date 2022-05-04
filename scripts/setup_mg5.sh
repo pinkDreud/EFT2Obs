703f3eaaf5c818208bb8744d49bf95fb19cef775
Matteo Magherini <50661613+pinkDreud@users.noreply.github.com>
2022-05-04 10:35:50 +0200

    Update setup_mg5.sh

--- scripts/setup_mg5.sh
+++ scripts/setup_mg5.sh
@@ -1,10 +1,31 @@
 #!/usr/bin/env bash
+set -e
+
+IWD=${PWD}
+source env.sh
+
+if [ -z "${LHAPDF_CONFIG_PATH}" ]; then echo "ERROR: environment variable LHAPDF_CONFIG_PATH is not set"; exit 1; fi
+if [ -z "${MG_DIR}" ]; then echo "ERROR: environment variable MG_DIR is not set"; exit 1; fi
+if [ -z "${MG_TARBALL}" ]; then echo "ERROR: environment variable MG_TARBALL is not set"; exit 1; fi
+
+LHAPDF_VERSION="LHAPDF-6.2.1"
+wget "https://lhapdf.hepforge.org/downloads/?f=${LHAPDF_VERSION}.tar.gz" -O "${LHAPDF_VERSION}.tar.gz"
+tar xf "${LHAPDF_VERSION}.tar.gz"
+rm "${LHAPDF_VERSION}.tar.gz"
+mkdir lhapdf
+pushd "${LHAPDF_VERSION}"
+	./configure --prefix="${IWD}/lhapdf/"
+	make
+	make install
+popd
+rm -r "${LHAPDF_VERSION}"
+
 
 if [ -d "${MG_DIR}" ]; then
   echo "Directory ${MG_DIR} already exists, remove this first to re-install"
   exit 1
 fi
-wget "https://launchpad.net/mg5amcnlo/2.0/2.6.x/+download/${MG_TARBALL}" --no-check-certificate
+wget "https://launchpad.net/mg5amcnlo/2.0/2.6.x/+download/${MG_TARBALL}"
 tar -zxf "${MG_TARBALL}"
 rm "${MG_TARBALL}"
 
